29e571c29c353b1fdbece2040930308a4d247b9b
Kenneth Endfinger <kaendfinger@gmail.com>
2014-12-23 20:39:03 -0500

    plz work

--- link.dart
+++ link.dart
@@ -15,6 +15,42 @@ void setupLink() {
         }, execute: (args) {
           bot.message(net, args['target'].toString(), args['message'].toString());
         });
+        
+        networkNode.createAction("Join", params: {
+          "channel": ValueType.STRING
+        }, execute: (args) {
+          bot.send("join", {
+            "channel": args["channel"].toString()
+          });
+        });
+        
+        networkNode.createAction("Part", params: {
+          "channel": ValueType.STRING
+        }, execute: (args) {
+          bot.send("join", {
+            "channel": args["channel"].toString()
+          });
+        });
+        
+        bot.get("whois", {
+          "network": net,
+          "user": "DirectCodeBot"
+        }).then((data) {
+          for (var channel in data["channels"]) {
+            var channelNode = networkNode.createChild(channel);
+            channelNode.createAction("SendMessage", params: {
+              "message": ValueType.STRING
+            }, execute: (args) {
+              bot.message(net, channel, args["message"].toString());
+            });
+            
+            channelNode.createAction("Part", execute: (args) {
+              bot.send("part", {
+                "channel": channel
+              });
+            });
+          }
+        });
       }
     });
   });
