94c0f4050e3baebe045408c449307af5f5cc80d1
Kenneth Endfinger <kaendfinger@gmail.com>
2015-01-20 00:14:09 -0500

    Add Stats for Active Users

--- commands.dart
+++ commands.dart
@@ -291,8 +291,11 @@ void handleCommand(CustomCommandEvent event) {
 
         if (users.isNotEmpty) {
           var most = users.first['name'];
+          var actives = (users.take(6).toList()..removeAt(0)).map((it) => it['name']);
+          var least = users.last['name'];
 
           event.replyNotice("Most Talkative User on ${event.channel}: ${most}", prefixContent: "Statistics");
+          event.replyNotice("Most Active Users on ${event.channel}: ${actives.join(", ")}", prefixContent: "Statistics");
         }
       }
       break;
